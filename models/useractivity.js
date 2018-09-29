79859e7599fd1284fdc879ae854552e58bf8bcb6
syin <shaomingyinut@gmail.com>
2018-09-29 12:19:04 -0400

    adding in basic html routes

--- models/useractivity.js
+++ models/useractivity.js
@@ -2,6 +2,8 @@ module.exports = function(sequelize, DataTypes) {
   var Activity = sequelize.define('Activity', {
     showsWatched: DataTypes.STRING,
     showsGenre: DataTypes.STRING,
+    showsActors: DataTypes.STRING,
+    showsDirector: DataTypes.STRING,
     showsRating: DataTypes.INTEGER
   });
   return Activity;
