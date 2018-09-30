90483f34131c205bfba3b5c875037c262259e237
syin <shaomingyinut@gmail.com>
2018-09-30 11:28:00 -0400

    updated get request to grab all watched shows

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,7 +12,7 @@ var db = require('../models');
 // =============================================================
 module.exports = function(app) {
   // GET route for getting all of the todos
-  app.get('/api/todos', function(req, res) {
+  app.get('/api/watchedShows', function(req, res) {
     // findAll returns all entries for a table when used with no options
     db.Activity.findAll({}).then(function(dbTodo) {
       // We have access to the todos as an argument inside of the callback function
