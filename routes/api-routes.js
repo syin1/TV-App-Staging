b8c90358a651427a917344b4349ad339de700e53
syin <shaomingyinut@gmail.com>
2018-09-30 16:44:10 -0400

    added movie display html & css template

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -45,16 +45,16 @@ module.exports = function(app) {
 
   // DELETE route for deleting todos. We can get the id of the todo to be deleted from
   // req.params.id
-  app.delete('/api/todos/:id', function(req, res) {
-    // We just have to specify which todo we want to destroy with "where"
-    db.Activity.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbTodo) {
-      res.json(dbTodo);
-    });
-  });
+  // app.delete('/api/todos/:id', function(req, res) {
+  //   // We just have to specify which todo we want to destroy with "where"
+  //   db.Activity.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   }).then(function(dbTodo) {
+  //     res.json(dbTodo);
+  //   });
+  // });
 
   // PUT route for updating todos. We can get the updated todo data from req.body
   app.put('/api/todos', function(req, res) {
